d518b1385f90be89866adf62fdeeab96042f82fe
Ashwini B S <ashsheshu97@gmail.com>
2018-03-14 15:23:22 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDOAfPidd4LC6tSu7tVCHX2T1vxAD2x5kY",
+    authDomain: "contactform-d8e8c.firebaseapp.com",
+    databaseURL: "https://contactform-d8e8c.firebaseio.com",
+    projectId: "contactform-d8e8c",
+    storageBucket: "",
+    messagingSenderId: "1023226263395"
+  };
+  firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
